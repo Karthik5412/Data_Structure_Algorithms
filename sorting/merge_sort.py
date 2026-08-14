5dc8a36d28cfc37ee1f824a6baa7db15a1ea4605
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-14 21:16:09 +0530

    optimization done for bubble sort

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -4,11 +4,14 @@ class merge_sort :
 class bubble_sort :
     def algo(self, arr : list[int]) -> list[int] :
         n = len(arr)
-        
         for i in range(n) :
+            flag = True
             for j in range(n-1-i) :
                 if arr[j] > arr[j+1] :
                     arr[j], arr[j+1] = arr[j+1], arr[j]
+                    flag = False
+            if flag :
+                break
                     
         return arr 
     
