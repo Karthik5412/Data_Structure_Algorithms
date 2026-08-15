5776556d6e7cbab391bee10332486619c12a4871
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-15 12:32:45 +0530

    Added new class

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -34,13 +34,32 @@ class selection_sort :
             
         return arr
 
+
+class insertion_sort :
+    def algo(self, arr : list[int]) -> list[int]:
+        n = len(arr)
+        
+        for i in range(1,n) :
+            pick = arr[i]
+            j = i-1
+
+            while j >=0 and arr[j] > pick :
+                arr[j+1] = arr[j]
+                j-= 1
+            arr[j+1] = pick
+            
+        return arr 
+
 arr = [8,3,2,6,12,1]
 
 bubble = bubble_sort()
 selection = selection_sort()
+insertion = insertion_sort()
 
 op1 = bubble.algo(arr)
 op2 = selection.algo(arr)
+op3 = insertion.algo(arr)
 
 print(op1)
 print(op2)
+print(op3)
