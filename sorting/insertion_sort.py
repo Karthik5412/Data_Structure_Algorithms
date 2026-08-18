f617a17cbb35b13a17c8aad1470b0efe0a3c4ff3
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-18 21:56:24 +0530

    Changes happend

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -46,10 +46,41 @@ class Sorting_algos :
             
         return arr
 
-
+    def merge_sort(self, arr: list[int]) -> list[int] :
+        
+        if len(arr) <= 1 :
+            return arr 
+        
+        mid = len(arr) // 2
+        
+        left = self.merge_sort(arr[:mid])
+        right = self.merge_sort(arr[mid:])
+        
+        return self.merge(left,right)
+    
+    def merge(self, left : list[int], right : list[int]) ->  list[int] :
+        i=j=0 
+        res = [] 
+        
+        while i < len(left) and j < len(right) :
+            if left[i] < right[j] :
+                res.append(left[i])
+                i+= 1
+            else :
+                res.append(right[j])
+                j+= 1
+                
+        res.extend(left[i:])
+        res.extend(right[j:])
+        
+        return res 
+        
+        
+        
 nums = [8,3,2,9,6,12,1]
 s = Sorting_algos()
 
 print(s.insertion_sort(nums))
 print(s.bubble_sort(nums))
 print(s.selection_sort(nums))
+print(s.merge_sort(nums))
