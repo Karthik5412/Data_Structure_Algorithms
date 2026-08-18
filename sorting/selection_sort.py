7147ce21caec185d506d20c30d434384b279e333
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-18 21:39:08 +0530

    some changes

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,20 +1,20 @@
 def selection_sort(arr : list[int]) -> list[int] :
 
     l = len(arr)
-
+    is_sorting = True
     for i in range(l-1) :
         min_idx = i
-        is_sorting = False
+        
 
         for j in range(i+1,l) :
             if arr[min_idx] > arr[j] :
                 min_idx= j
-                is_sorting = True
+                is_sorting = False
 
-        if not is_sorting :
+        if is_sorting :
             break
-        if min_idx != i :
-            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
     return arr
 
@@ -59,13 +59,29 @@ def merge_sort(arr : list[int]) -> list[int] :
     merge(left,right)
 
 def merge(left, right) :
-    pass
+    i = j = 0
+    ans = []
+    while i < len(left) and j < len(right):
+        if left[i] < right[j] :
+            ans.append(left[i])
+            i+=1
+        else :
+            ans.append(right[j])
+            j+= 1
+            
+    ans.extend(left[i:])
+    ans.extend(right[j:])
+    
+    return ans 
 
-nums = [8,3,2,9,6,12,1]
+n1 = [8,3,2,9,6,12,1]
+n2 = [8,3,2,9,6,12,1]
+n3 = [8,3,2,9,6,12,1]
+n4 = [8,3,2,9,6,12,1]
 
-selection = selection_sort(nums)
-bubble = bubble_sort(nums)
-insertion = insertion_sort(nums)
+selection = selection_sort(n1)
+bubble = bubble_sort(n2)
+insertion = insertion_sort(n3)
 
 print(f'bubble sort    --> {bubble}')
 print(f'selection sort --> {selection}')
