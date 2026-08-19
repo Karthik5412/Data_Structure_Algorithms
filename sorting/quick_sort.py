b06331ee613db05e3d3eddd2d4d3aac525ad2771
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-19 11:27:03 +0530

    Done with selection_sort

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,15 +1,32 @@
 def bubble(arr) :
     n = len(arr) 
-    
     for i in range(n) :
+        flag = True
         for j in range(n-i-1) :
             if arr[j] > arr[j+1] :
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+                flag = False 
+                
+        if flag :
+            break
                 
     return arr
     
-def selection() :
-    pass
+def selection(arr) :
+    n = len(arr)
+    for i in range(n) :
+        flag = True
+        idx = i 
+        for j in range(i+1, n) :
+            if arr[idx] > arr[j] :
+                idx = j 
+                flag = False 
+                
+        if flag or idx == i:
+            continue
+        arr[i], arr[idx] = arr[idx], arr[i]
+        
+    return arr 
 
 def insretion() :
     pass 
@@ -19,4 +36,5 @@ def merge() :
 
 arr = [8,3,2,6,12,1] 
 
-print(bubble(arr))
+print(arr)
+print(selection(arr))
