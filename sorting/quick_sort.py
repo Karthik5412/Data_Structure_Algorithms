02d0a49017fe7cb645e86edc81117d7dc23a852d
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-19 11:46:44 +0530

    Done with quick

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -70,8 +70,18 @@ def algo(left,right) :
     res.extend(right[j:])
     return res 
     
+def quick(arr) :
+    if len(arr) <= 1 :
+        return arr 
+    
+    pivot = arr[-1]
+    
+    left = [x for x in arr[:-1] if x <= pivot]
+    right = [x for x in arr[:-1] if x > pivot]
+    
+    return quick(left) + [pivot] + quick(right) 
 
 arr = [8,3,2,6,12,1] 
-res = merge(arr)
+res = quick(arr)
 print(arr) 
 print(res)
