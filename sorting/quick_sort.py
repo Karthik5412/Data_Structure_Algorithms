33b63845528f968793046dd8dc1e2f9b5e7c49d8
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-19 11:14:05 +0530

    bubble_sort is done

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,5 +1,12 @@
-def bubble() :
-    pass 
+def bubble(arr) :
+    n = len(arr) 
+    
+    for i in range(n) :
+        for j in range(n-i-1) :
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                
+    return arr
     
 def selection() :
     pass
@@ -11,3 +18,5 @@ def merge() :
     pass 
 
 arr = [8,3,2,6,12,1] 
+
+print(bubble(arr))
