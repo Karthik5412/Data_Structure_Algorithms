efc15e75bf41e45fcd9b9e7f1f62bcbcab5bf7c5
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-20 14:19:10 +0530

    Created a LinkedList class

--- linked_list/linked_list_1.py
+++ linked_list/linked_list_1.py
@@ -4,5 +4,39 @@ class Node :
         self.next = None 
         
 class Linked_List :
+    def __init__(self) :
+        self.head = None 
     
+    def insertion(self,data) :
+        new_node = Node(data) 
+        
+        if not self.head :
+            self.head = new_node 
+            
+        else :
+            current = self.head 
+            
+            while current.next != None :
+                current = current.next 
+                
+            current.next = new_node 
+            
+    def display(self) :
+        if not self.head :
+            print('Noting')
+            
+        else :
+            current = self.head 
+            while current.next :
+                print(f'{current.data}->',end='')
+                current = current.next 
+            print(current.data)
+        
+l1 = Linked_List()
+
+l1.display()
+l1.insertion(10)
+l1.insertion(20)
+l1.insertion(30)
+l1.display()
         
