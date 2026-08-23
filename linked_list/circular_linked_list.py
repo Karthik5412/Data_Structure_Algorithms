ae184b838e99a231e9e0df702407b4232e575aad
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-23 21:19:16 +0530

    Added new function for inserting node in front

--- linked_list/circular_linked_list.py
+++ linked_list/circular_linked_list.py
@@ -26,7 +26,24 @@ class Circular_Linked_List :
                 current.next = new_node 
                 
                 self.size += 1
+            
+    def insert_at_front(self,data) :
+            new_node = Node(data) 
+            self.size += 1
+            if not self.head :
+                self.head = new_node
+                self.head.next = self.head 
+            else :
+                new_node.next = self.head 
                 
+                curr = self.head 
+                
+                while curr.next != self.head  :
+                    curr = curr.next 
+                
+                curr.next = new_node 
+                self.head = new_node 
+    
     def display(self) :
             if not self.head :
                 print('Noting')
@@ -45,5 +62,12 @@ cl.insertion(10)
 cl.insertion(20)
 cl.insertion(30)
 cl.insertion(40)
+
+cl.insert_at_front(120)
+
+cl.insertion(50)
+cl.insertion(60)
+cl.insertion(70)
+cl.insert_at_front(80)
 
 cl.display()
