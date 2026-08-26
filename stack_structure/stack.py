98407ad0f37b5ccf461cdc9c2b8feabd524c3b42
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-26 11:16:10 +0530

    Added new function is_empty

--- stack_structure/stack.py
+++ stack_structure/stack.py
@@ -8,19 +8,24 @@ class Stack :
         
     
     def peak(self) :
-        if self.stack :
+        if not self.is_empty :
             print(f"peak element is {self.stack[-1]}")
         else :
             print('stack is empty')
     
+    def is_empty(self) :
+        return len(self.stack) == 0
     
 stack = Stack()
 
 stack.peak()
+print(stack.is_empty())
 
 stack.push(10)
 stack.push(20)
 stack.peak()
+
+print(stack.is_empty())
 
 stack.push(30)
 stack.push(40)
