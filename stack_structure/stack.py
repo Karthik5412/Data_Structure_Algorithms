b21ea7b1b06c98e409d3326ca7fc078daa036f42
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-26 11:13:47 +0530

    Created peak function

--- stack_structure/stack.py
+++ stack_structure/stack.py
@@ -7,13 +7,25 @@ class Stack :
         print(f'{ele} is added to stack')
         
     
+    def peak(self) :
+        if self.stack :
+            print(f"peak element is {self.stack[-1]}")
+        else :
+            print('stack is empty')
     
     
 stack = Stack()
 
+stack.peak()
+
 stack.push(10)
 stack.push(20)
+stack.peak()
+
 stack.push(30)
 stack.push(40)
+stack.peak()
+
 stack.push(50)
 
+stack.peak()
