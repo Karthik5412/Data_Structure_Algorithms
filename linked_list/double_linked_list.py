6c9ad95b651b32e7ab0d6241c22948f6b1bd8534
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-27 15:41:08 +0530

    Just created Node

---
+++ linked_list/double_linked_list.py
@@ -0,0 +1,12 @@
+class Node :
+    def __init__(self, val):
+        self.val = val 
+        self.prev = None 
+        self.next = None 
+
+class DoubleLinkedList :
+    def __init__(self):
+        self.head = None
+        self.size 
+        
+    
