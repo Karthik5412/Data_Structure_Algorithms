ecef4d6de8dd7849dcb2ad579ac47e2d9c6286e8
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-27 15:44:50 +0530

    created new function called insertion

--- linked_list/double_linked_list.py
+++ linked_list/double_linked_list.py
@@ -9,4 +9,19 @@ class DoubleLinkedList :
         self.head = None
         self.size 
         
-    
+    def insertion(self,val) :
+        new_node = Node(val)
+        
+        if not self.head :
+            self.head = new_node
+            self.size += 1
+        else :
+            curr = self.head 
+            
+            while curr :
+                curr = curr.next 
+                
+            curr.next = new_node
+            new_node.prev = curr
+            self.size += 1
+            
