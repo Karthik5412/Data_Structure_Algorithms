d9d8b8d1c1c5d3240475c7c8981ef9e9d523dadf
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-27 15:56:04 +0530

    Created tail to display

--- linked_list/double_linked_list.py
+++ linked_list/double_linked_list.py
@@ -7,21 +7,59 @@ class Node :
 class DoubleLinkedList :
     def __init__(self):
         self.head = None
-        self.size 
+        self.tail = None
+        self.size = 0
         
     def insertion(self,val) :
         new_node = Node(val)
         
         if not self.head :
             self.head = new_node
+            self.tail = new_node
             self.size += 1
         else :
             curr = self.head 
             
-            while curr :
+            while curr.next != None:
                 curr = curr.next 
                 
             curr.next = new_node
             new_node.prev = curr
+            self.tail = new_node
             self.size += 1
             
+    def display(self,reverse = False) :
+        
+        if not self.head :
+            print('Empty')
+            return
+        
+        if reverse :
+            curr = self.tail 
+            
+            while curr.prev :
+                print(f'{curr.val} <=>', end=' ')
+                curr = curr.prev
+            print(curr.val)
+            
+            
+        else :
+            curr = self.head 
+            
+            while curr.next :
+                print(f'{curr.val} <=>', end=' ')
+                curr = curr.next
+            print(curr.val)
+
+
+
+
+dl = DoubleLinkedList()
+
+dl.insertion(10)
+dl.insertion(20)
+dl.insertion(30)
+dl.insertion(40)
+print(dl.tail.val)
+
+dl.display(reverse=True)
