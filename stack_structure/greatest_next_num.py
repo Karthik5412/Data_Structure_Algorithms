4e6f0db9d49460e11b83e552fe03eb227ef21b12
Karthik5412 <kkuricharlapati@gmail.com>
2026-08-30 13:48:46 +0530

    Done with this program

--- stack_structure/greatest_next_num.py
+++ stack_structure/greatest_next_num.py
@@ -1,13 +1,18 @@
 def next_great_num(arr) :
     stack = []
     res = [-1] * len(arr)
-    for idx , num in enumerate(arr) :
+    for idx , num in enumerate(arr[::-1]) :
         while  stack and stack[-1] <= num :
             stack.pop()
-        
-        res[idx] = num
+        if stack :
+            res[idx] = stack[-1]
         
         stack.append(num)
         
-    return res 
+    return res[::-1]
 
+arr = [4, 5, 2, 25, 7, 8, 1, 20, 11, 3, 12, 6]
+
+print(next_great_num(arr))
+
+print([5, 25, 25, -1, 8, 20, 20, -1, 12, 12, -1, -1])
