3950ffb9e1f87774d749be666f5eaa4d82638273
Karthik5412 <kkuricharlapati@gmail.com>
2026-09-03 13:42:50 +0530

    Some checking

--- queue_structure/queue.py
+++ queue_structure/queue.py
@@ -2,21 +2,32 @@ class Queue :
     def __init__(self) :
         self.queue = []
     
-    def enqueue(self, val) :
+    def enque(self, val) :
         self.queue.append(val)
     
-    def dequeue(self) :
+    def deque(self) :
             self.queue.pop(0)
         
     def peak(self) :
-        return self.queue[0]
+        print(self.queue[0])
     
     
 que = Queue()
 
 
 que.enque(10)
+que.peak()
+
 que.enque(20)
+que.peak()
+
 que.enque(30)
 que.enque(40)
+que.peak()
+
 que.enque(50)
+
+
+
+que.deque()
+que.peak()
