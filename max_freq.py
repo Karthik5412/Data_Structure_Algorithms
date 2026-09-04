f4b49bb79bff6429b38e25bc304c8e42310af416
Karthik5412 <kkuricharlapati@gmail.com>
2026-09-04 20:04:40 +0530

    Created Push operation

--- max_freq.py
+++ max_freq.py
@@ -4,3 +4,14 @@ class FreqStack:
         self.count = {}
         self.grp = {}
         self.mexFreq = 0
+        
+    def push(self, val: int) -> None:
+        self.count[val] = self.count.get(val,0) + 1
+        freq = self.count[val]
+
+        if not self.grp[freq] :
+            self.grp[freq] = []
+
+        self.grp[freq].append(val)
+
+        self.maxFreq = max(freq, self.maxFreq)
