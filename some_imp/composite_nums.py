020c85cfe327a8199c4cab2ae185a4ecc571dc42
Karthik5412 <kkuricharlapati@gmail.com>
2026-09-08 19:47:07 +0530

    basic code for prime nums

---
+++ some_imp/composite_nums.py
@@ -0,0 +1,14 @@
+def prime_nums(limit : int) :
+    nums = [True] * (limit+1)
+    
+    nums[0] = nums[1] = False 
+    
+    for n in range(2,limit+1) :
+        if nums[n] == True :
+            for i in range(n*n , limit+1, n) :
+                nums[i] = False 
+                
+    return [i for i, val in enumerate(nums) if  val] 
+
+
+print(prime_nums(100))
