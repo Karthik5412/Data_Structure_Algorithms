11755e699ffc71b085d7216b374f58e3fe072f78
Karthik5412 <kkuricharlapati@gmail.com>
2026-09-10 17:51:42 +0530

    sqr root

--- some_imp/square_root.py
+++ some_imp/square_root.py
@@ -1,5 +1,20 @@
 def square_root(num) :
-    pass 
-
+    low = 1
+    high = num // 2 
+    ans = 0
+    
+    while low <= high :
+        mid = (low + high) // 2
+        sqr = mid * mid
+        
+        if num == sqr :
+            return mid
+        elif sqr < num :
+            ans = mid 
+            low = mid + 1
+        else :
+            high = mid -1
+            
+        return ans
 
 print(square_root(144))
