489512fcca50ec439c267975038462808bb0979d
Karthik5412 <kkuricharlapati@gmail.com>
2026-09-13 13:10:17 +0530

    prblm 3

--- recussion/even_and_odd.py
+++ recussion/even_and_odd.py
@@ -7,4 +7,14 @@ def even_num(end,curr = 0,res=[]) :
     return even_num(end,curr + 2,res)
 
 
+def odd_num(end,curr=1,res=[]) :
+    if curr > end :
+        return res 
+    
+    res.append(curr)
+    
+    return odd_num(end,curr+2,res)
+
 print(even_num(100))
+print()
+print(odd_num(100))
