ea3f065fe230ae741fc4ebc82bc656c619f0ca39
Karthik5412 <kkuricharlapati@gmail.com>
2026-09-14 19:07:41 +0530

    prblm 10

--- recussion/is_sorted.py
+++ recussion/is_sorted.py
@@ -1,14 +1,27 @@
-def is_sorted(arr,n=None) :
+def is_sorted_asc(arr,n=None) :
     
     if n == None :
-        n = len(arr) - 1
+        n = len(arr) 
         
     
     if n == 1 :
         return True
-    elif arr[n] < arr[n-1] :
+    elif arr[n-1] < arr[n-2] :
             return False 
     
-    return arr[n] >= arr[n-1] and is_sorted(arr,n-1)
+    return arr[n-1] >= arr[n-2] and is_sorted_asc(arr,n-1)
 
-print(is_sorted([1]))
+
+def is_sorted_des(arr, n=None) :
+    if n == None:
+        n = len(arr)
+        
+    if n == 1 :
+        return True 
+    
+    return arr[n-2] >= arr[n-1] and is_sorted_des(arr,n-1)
+
+print(is_sorted_asc([2, 4, 6, 8]))
+print(is_sorted_asc([2, 4, 6, 8][::-1]))
+print(is_sorted_des([2, 4, 6, 8]))
+print(is_sorted_des([2, 4, 6, 8][::-1]))
