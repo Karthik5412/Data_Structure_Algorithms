77f1a766b92a62531f88e5d5ac3b3d38d563bdd9
Karthik5412 <kkuricharlapati@gmail.com>
2026-09-24 22:06:09 +0530

    Linear search with inner function

--- recussion/linear_Search.py
+++ recussion/linear_Search.py
@@ -8,10 +8,28 @@ def linear_search(arr,target,idx=0) :
     
     return linear_search(arr,target,idx+1)
     
+    
+def search(arr,target) :
+    
+    def ls(idx) :
+        if idx >= len(arr) :
+            return -1 
+        
+        if arr[idx] == target :
+            return idx 
+        
+        
+        return ls(idx+1)
+    
+    return ls(0)
 
 n1 = [8,3,2,6,12,1] 
 
 print(linear_search(n1,8))
+
+res = search(n1,8)
+
+print(res)
 
 
 
