aad11ecc7b0d086df25a0b983ca82f07f1be1679
Karthik5412 <kkuricharlapati@gmail.com>
2026-09-24 21:16:35 +0530

    a recursion for linear_search

---
+++ recussion/linear_Search.py
@@ -0,0 +1,14 @@
+def linear_search(arr,target,idx=0) :
+    
+    if idx == len(arr) :
+        return -1
+    
+    if arr[idx] == target :
+        return idx
+    
+    return linear_search(arr,target,idx+1)
+    
+
+n1 = [8,3,2,6,12,1] 
+
+print(linear_search(n1,8))
