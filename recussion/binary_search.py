5695d2469073140f91fa1fbcd5ba7847635e22bb
Karthik5412 <kkuricharlapati@gmail.com>
2026-09-25 21:31:52 +0530

    Binary

--- recussion/binary_search.py
+++ recussion/binary_search.py
@@ -1,4 +1,19 @@
 def binary_search(arr,target) :
     
     def search(left,right) :
-        pass
+        
+        if left > right :
+            return -1
+        
+        mid = left + (right - left) // 2 
+        
+        if arr[mid] == target :
+            return mid 
+        elif arr[mid] > target :
+            return search(left,mid-1)
+        else :
+            return search(mid+1,right)
+        
+    return search(0,len(arr)-1)
+    
+    
